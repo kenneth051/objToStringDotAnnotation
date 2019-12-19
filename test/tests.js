530b5143f0eab1785166ac573da41e65b94443ca
kenneth051 <kjdumba@gmail.com>
2019-12-19 20:26:10 +0300

    update readme file

--- test/tests.js
+++ test/tests.js
@@ -6,17 +6,17 @@ expect = chai.expect
 
 describe("dotnotate object tests", function () {
     it("should return an object after turning a nested object in a dot notation", function () {
-        const obj = dot.objNotateToString(test_data.data);
+        const obj = dot(test_data.data);
         expect(obj).to.eql(test_data.data_results)
 
     });
     it("should return an empty object if empty object is passed in", function () {
-        const obj = dot.objNotateToString(test_data.empty_obj);
+        const obj = dot(test_data.empty_obj);
         expect(obj).to.eql(test_data.empty_obj)
 
     });
     it("should return dot notated object regardless if one attribute has an array as a value", function () {
-        const obj = dot.objNotateToString(test_data.array_value);
+        const obj = dot(test_data.array_value);
         expect(obj).to.eql(test_data.array_value_result)
 
     });
